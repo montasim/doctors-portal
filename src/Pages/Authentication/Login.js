9d66afb10c1d6d24f51a453e081c298eb2d0ac60
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-18 10:15:59 +0600

    installed react form hook

--- src/Pages/Authentication/Login.js
+++ src/Pages/Authentication/Login.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            <h2>Login</h2>
+        <div className='flex h-screen'>
+            <div className='m-auto card w-96 bg-base-100 shadow-xl'>
+                <div className='card-body'>
+                    <h2 className='text-3xl text-center font-bold'>Login</h2>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
