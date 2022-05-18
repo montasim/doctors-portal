88740c0ffb5c414c574370420b1df028e8ad1e45
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-18 15:52:13 +0600

    route added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import { Route, Routes } from "react-router-dom";
+import About from "./Pages/About/About";
 import Login from "./Pages/Authentication/Login";
 import Signup from "./Pages/Authentication/Signup";
+import Blog from "./Pages/Blog/Blog";
+import Contact from "./Pages/Contact/Contact";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <div>
       <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
       </Routes>
