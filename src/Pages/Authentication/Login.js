564310b8ee377cd4c5b9f1fcc2684dedaf52225b
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-18 10:38:57 +0600

    login page created

--- src/Pages/Authentication/Login.js
+++ src/Pages/Authentication/Login.js
@@ -1,13 +1,81 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+
     return (
         <div className='flex h-screen'>
             <div className='m-auto card w-96 bg-base-100 shadow-xl'>
                 <div className='card-body'>
                     <h2 className='text-3xl text-center font-bold'>Login</h2>
                     <div>
+                        <form onSubmit={handleSubmit(onSubmit)}>
 
+                            <div className="form-control w-full max-w-xs">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input
+                                    type="email"
+                                    placeholder="Your Email"
+                                    className="input input-bordered w-full max-w-xs"
+                                    {...register("email", {
+                                        required: {
+                                            value: true,
+                                            message: 'Email is Required'
+                                        },
+                                        pattern: {
+                                            value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                            message: 'Provide a valid Email'
+                                        }
+                                    })}
+                                />
+                                <label className="label">
+                                    {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                                    {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                                </label>
+                            </div>
+                            <div className="form-control w-full max-w-xs">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input
+                                    type="password"
+                                    placeholder="Password"
+                                    className="input input-bordered w-full max-w-xs"
+                                    {...register("password", {
+                                        required: {
+                                            value: true,
+                                            message: 'Password is Required'
+                                        },
+                                        minLength: {
+                                            value: 6,
+                                            message: 'Must be 6 characters or longer'
+                                        }
+                                    })}
+                                />
+                                <label className="label">
+                                    {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                                    {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                                </label>
+                            </div>
+
+                            <input className='btn w-full max-w-xs text-white' type="submit" value="Login" />
+                        </form>
+
+
+                        <p className='mt-4'><small>New to Doctors Portal? <Link className='text-primary' to="/signup">Create New Account</Link></small></p>
+
+                        <p className='mt-2'><small>Forgot Password? <Link className='text-primary' to="/reset-password">Reset Password</Link></small></p>
+
+                        <div className="divider">OR</div>
+
+                        <button
+                            className="btn btn-accent w-full max-w-xs text-white"
+                        >Continue with Google</button>
                     </div>
                 </div>
             </div>
