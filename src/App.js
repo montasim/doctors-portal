f378b048189a841ee11c9c4ef1a56c9f5bfe394e
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-18 10:45:12 +0600

    signup page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./Pages/Authentication/Login";
+import Signup from "./Pages/Authentication/Signup";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
     </div>
   );
